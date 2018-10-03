7236d06522ae3f83addebcfd472be081958c66c2
kartik28 <kartiknagpal28@gmail.com>
2018-10-02 21:37:07 -0400

    add comment for javadoc

--- src/main/java/gui/CountryPanel.java
+++ src/main/java/gui/CountryPanel.java
@@ -1,25 +1,24 @@
 package gui;
 
-import entity.Country;
-
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.util.HashMap;
 
 import static javax.swing.BoxLayout.Y_AXIS;
+
+/**
+ * Class containing fuctions and GUI for the country panel
+ **/
 
 public class CountryPanel extends JPanel {
 
     public CountryPanel() {
         setBackground(Color.LIGHT_GRAY);
         setBorder(new LineBorder(Color.BLACK, 2));
-        setLayout(new BoxLayout( this, Y_AXIS));
-        String labels[] = { "India", "Pakistan", "China", "Afganistan","Nepal", "Bangladesh", "Sri Lanka", "Malasyia","Mangol", "Russia" };
+        setLayout(new BoxLayout(this, Y_AXIS));
+        String labels[] = {"India", "Pakistan", "China", "Afganistan", "Nepal", "Bangladesh", "Sri Lanka", "Malasyia", "Mangol", "Russia"};
         JList list = new JList(labels);
         JScrollPane scrollPane12 = new JScrollPane(list);
         add(scrollPane12);
     }
-
-
 }
