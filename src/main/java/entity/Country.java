86fd257b11beedbbec3113cac421cfd999532c65
rashmi <rashmi.manandhar7@gmail.com>
2018-10-05 16:43:55 -0400

    get neighbouring countries added to country model

--- src/main/java/entity/Country.java
+++ src/main/java/entity/Country.java
@@ -1,5 +1,10 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class Country {
     public int id;
     public String name;
@@ -13,6 +18,21 @@ public class Country {
 
     @Override
     public String toString() {
-        return id + ". " +name;
+        return id + ". " + name;
     }
+
+    /**
+     * Get the list of Neighbour countries
+     *
+     * @param map HashMap of country id and set of countries
+     * @return list of countries
+     */
+    public List<Country> getNeighbours(HashMap<Integer, HashSet<Country>> map) {
+        List<Country> countries = new ArrayList<>();
+        for (Country country : map.get(this.id)) {
+            countries.add(country);
+        }
+        return countries;
+    }
+
 }
