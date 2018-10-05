a3f23592de53de38d7414ca28015366257b9d7f9
Julian Raj Manandhar <julianrajman@gmail.com>
2018-10-05 16:20:02 -0400

    add validation to map form

--- src/main/java/controller/MapCreatorController.java
+++ src/main/java/controller/MapCreatorController.java
@@ -7,10 +7,6 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -29,7 +25,67 @@ public class MapCreatorController extends BaseController<MapCreatorFrame> implem
 
     @Override
     public void actionPerformed(ActionEvent event) {
+        try {
+            String mapName = view.mapName.getText();
+            HashSet<String> continentCheckList = new HashSet<>();
+            HashSet<String> countryCheckList = new HashSet<>();
+            HashSet<String> neighboursCheckList = new HashSet<>();
 
+            ArrayList<String> continents = new ArrayList<>();
+            for (JTextField field : view.getContinentFields()) {
+                String text = field.getText().trim();
+                if (!text.isEmpty()) {
+                    String[] data = text.replace(" ", "").split("=");
+
+                    if (data.length != 2) throw new IllegalStateException("CV not provided");
+                    int cv = Integer.valueOf(data[1]);
+
+                    if (!continentCheckList.add(data[0]))
+                        throw new IllegalArgumentException("Same continent name added twice");
+
+                    continents.add(text);
+                }
+            }
+
+            ArrayList<String> countries = new ArrayList<>();
+            for (JTextField field : view.getCountryFields()) {
+                String text = field.getText().trim();
+                if (!text.isEmpty()) {
+                    String[] data = text.replace(" ", "").split(",");
+
+
+                    if (data.length <= 1)
+                        throw new IllegalStateException("No continent provided for a country");
+                    if (data.length <= 2)
+                        throw new IllegalStateException("No neighbours provided for a country");
+                    if (!countryCheckList.add(data[0]))
+                        throw new IllegalArgumentException("Same country name added twice");
+                    if (!continentCheckList.contains(data[1]))
+                        throw new IllegalStateException("Invalid continent associated with a country.");
+
+                    for (int i = 2; i < data.length; i++) {
+                        neighboursCheckList.add(data[i]);
+                    }
+                    countries.add(text);
+                }
+            }
+
+            if (continents.isEmpty() || countries.isEmpty())
+                throw new IllegalStateException("No data provided");
+
+            if (countryCheckList.size() != neighboursCheckList.size())
+                throw new IllegalStateException("Not all the country has a neighbour");
+
+            //saveMapToFile(mapName, continents, countries);
+
+        } catch (NumberFormatException nfe) {
+            view.showWarning("Not a valid number");
+        } catch (IllegalStateException ise) {
+            view.showWarning(ise.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            view.showWarning(e.getMessage());
+        }
     }
 
     @Override
@@ -73,3 +129,18 @@ public class MapCreatorController extends BaseController<MapCreatorFrame> implem
         }
     }
 }
+
+/*
+Sample inputs for file creation
+
+Asia=3
+Europe=6
+
+Nepal, Asia, India,Germany,Portugal
+India, Asia, Nepal, Spain
+Spain,Europe,India,Portugal
+Portugal,Europe,Nepal,Spain,Italy
+Italy,Europe,Portugal,Spain
+Germany,Europe,Spain,Nepal
+
+*/
