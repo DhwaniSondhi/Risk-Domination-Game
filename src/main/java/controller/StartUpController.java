cafe084ca042b1069c4dd9f774c0572691dab07b
Julian Raj Manandhar <julianrajman@gmail.com>
2018-11-19 21:02:32 -0500

    assign random number of armies to computer players

--- src/main/java/controller/StartUpController.java
+++ src/main/java/controller/StartUpController.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.HashSet;
+import java.util.Random;
 
 /**
  * This class contain functionality to start up game
@@ -51,13 +52,34 @@ public class StartUpController extends BaseController<StartUpFrame> implements A
                 PlayerStrategy.Strategy strategy = (PlayerStrategy.Strategy) ((JComboBox<PlayerStrategy.Strategy>) c[1]).getSelectedItem();
                 Player player = new Player(index, name, strategy);
                 model.players.put(player.id, player);
-                if(strategy != PlayerStrategy.Strategy.HUMAN)
+                if (strategy != PlayerStrategy.Strategy.HUMAN)
                     completedPlayers.add(player.id);
                 index++;
             }
 
             model.assignCountriesToPlayers();
-            changeCurrentPlayer();
+            for (Integer playerId : completedPlayers) {
+                int availableArmy = model.getInitialArmy();
+                Player player = model.players.get(playerId);
+                int remainingCountries = player.countries.size();
+                index = 0;
+                for (Country country : model.players.get(playerId).countries) {
+                    int maxCount = availableArmy - remainingCountries + 1;
+                    if (index == player.countries.size() - 1) {
+                        country.setNumOfArmies(maxCount);
+                    } else {
+                        int random = new Random().nextInt(maxCount) + 1;
+                        country.setNumOfArmies(random);
+                        availableArmy -= random;
+                        remainingCountries--;
+                    }
+                    index++;
+                }
+            }
+            if (!changeCurrentPlayer()) {
+                stateChangeListener.onStartUpCompleted();
+                view.dispose();
+            }
         } else if (e.getActionCommand().equalsIgnoreCase("assign")) {
             if (view.playerCountries.getSelectedItem() != null) {
                 Country country = (Country) view.playerCountries.getSelectedItem();
