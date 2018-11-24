4b1714a47fd241a5a623f808907b7c7f330f2844
NishantSaini <nishantsainins@gmail.com>
2018-11-24 14:31:04 -0500

    Added gui for number of player and player strategy in tournament mode

--- src/main/java/controller/TournamentModeController.java
+++ src/main/java/controller/TournamentModeController.java
@@ -2,10 +2,17 @@ package controller;
 
 import view.TournamentModeFrame;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 /**
  * Controller class for tournament mode
  */
-public class TournamentModeController extends BaseController<TournamentModeFrame> {
+public class TournamentModeController extends BaseController<TournamentModeFrame>implements ItemListener, ActionListener {
+    int numberOfPlayer;
     /**
      * This is the constructor for the Controller
      *
@@ -13,5 +20,47 @@ public class TournamentModeController extends BaseController<TournamentModeFrame
      */
     public TournamentModeController(TournamentModeFrame view) {
         super(view);
+    }
+
+    /**
+     * Invoked when an item has been selected or deselected by the user.
+     * The code written for this method performs the operations
+     * that need to occur when an item is selected (or deselected).
+     *
+     * @param e
+     */
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        System.out.println("d");
+        if (e.getStateChange() == ItemEvent.SELECTED) {
+            System.out.println("fs");
+            JComboBox jComboBox = (JComboBox) e.getSource();
+            numberOfPlayer = (int) jComboBox.getSelectedItem();
+        }
+
+    }
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+        if (e.getActionCommand().equalsIgnoreCase("Submit")){
+            System.out.println("dsfs");
+            if(numberOfPlayer==0){
+                view.updatePlayersPanel(2);
+            }
+            else{
+                view.updatePlayersPanel(numberOfPlayer);
+            }
+
+        }
+        else if(e.getActionCommand().equalsIgnoreCase("Proceed")){
+
+        }
+
     }
 }
--- src/main/java/view/TournamentModeFrame.java
+++ src/main/java/view/TournamentModeFrame.java
@@ -1,10 +1,13 @@
 package view;
 
 import controller.TournamentModeController;
+import javafx.scene.control.ComboBox;
+import utility.strategy.PlayerStrategy;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.List;
 
 /**
  * Gui class for tournament view
@@ -18,13 +21,16 @@ public class TournamentModeFrame extends JFrame {
     JPanel jPanelForStrategyOfPlayer;
     JPanel jPanelForMaps;
     JPanel jPanelForNumberOfGamesOnMap;
+    JPanel mainPanel;
+
 
     /**
      * This is constructor for tournament view
      *This setup panel for view
      */
     public TournamentModeFrame(){
-        JPanel mainPanel=new JPanel();
+        tournamentModeController=new TournamentModeController(this);
+        mainPanel=new JPanel();
         gridBagLayoutMain = new GridBagLayout();
         bagConstraintsMain = new GridBagConstraints();
         mainPanel.setLayout(gridBagLayoutMain);
@@ -43,11 +49,91 @@ public class TournamentModeFrame extends JFrame {
         bagConstraintsMain.gridx = 0;
         bagConstraintsMain.gridy = 0;
         mainPanel.add(jLabelFortify, bagConstraintsMain);
-
+        playerPanels();
 
 
 
     }
+    public void playerPanels(){
+        jPanelForPlayer=new JPanel();
+        JButton jButtonForNumberOfPlayers=new JButton();
+        jButtonForNumberOfPlayers.setText("Submit");
+        bagConstraintsMain.fill = GridBagConstraints.VERTICAL;
+        bagConstraintsMain.gridx = 0;
+        bagConstraintsMain.gridy = 1;
+        mainPanel.add(jPanelForPlayer,bagConstraintsMain);
+
+        jPanelForPlayer.add(new JLabel("Number Of Players:"));
+        JComboBox<Integer> comboBoxNumberOfPlayer=new JComboBox<>();
+        for(int i=2;i<=4;i++){
+            comboBoxNumberOfPlayer.addItem(i);
+        }
+        jPanelForPlayer.add(comboBoxNumberOfPlayer);
+        comboBoxNumberOfPlayer.setName("NumberOfPlayer");
+
+        comboBoxNumberOfPlayer.addItemListener(tournamentModeController);
+        jButtonForNumberOfPlayers.addActionListener(tournamentModeController);
+        jPanelForPlayer.add(jButtonForNumberOfPlayers);
+
+        mainPanel.revalidate();
+        revalidate();
+
+    }
+
+    /**
+     * Shows player fields in the panel
+     *
+     * @param count number of players
+     */
+    public void updatePlayersPanel(int count) {
+        jPanelForStrategyOfPlayer=new JPanel();
+        int currentSize = jPanelForStrategyOfPlayer.getComponents().length;
+        int diff = Math.abs(currentSize - count);
+        if (currentSize <= count) {
+            for (int i = 0; i < diff; i++) {
+                int x = jPanelForStrategyOfPlayer.getComponentCount() + 1;
+                JPanel panel = new JPanel();
+                panel.setLayout(new FlowLayout());
+
+                JTextField nameField = new JTextField();
+                nameField.setText("Player" + x);
+                panel.add(nameField);
+
+                JComboBox<PlayerStrategy.Strategy> type = new JComboBox<>(PlayerStrategy.Strategy.values());
+                panel.add(type);
+
+                jPanelForStrategyOfPlayer.add(panel);
+            }
+        } else {
+            for (int i = currentSize - 1; i > currentSize - (diff + 1); i--) {
+                jPanelForStrategyOfPlayer.remove(i);
+            }
+        }
+
+
+        bagConstraintsMain.fill = GridBagConstraints.VERTICAL;
+        bagConstraintsMain.gridx = 0;
+        bagConstraintsMain.gridy = 2;
+        mainPanel.add(jPanelForStrategyOfPlayer,bagConstraintsMain);
+        mainPanel.revalidate();
+        mainPanel.repaint();
+        jPanelForStrategyOfPlayer.revalidate();
+        jPanelForStrategyOfPlayer.repaint();
+        jPanelForPlayer.setVisible(false);
+        JPanel jPanelProceedButton=new JPanel();
+        bagConstraintsMain.fill = GridBagConstraints.VERTICAL;
+        bagConstraintsMain.gridx = 0;
+        bagConstraintsMain.gridy = 3;
+        mainPanel.add(jPanelProceedButton,bagConstraintsMain);
+        JButton jButtonProceed=new JButton("Proceed");
+        jPanelProceedButton.add(jButtonProceed);
+        jButtonProceed.addActionListener(tournamentModeController);
+
+
+    }
+
+
+    
 
 
 }
